00da88787d4b2567a680846616479aa3d3a7ced8
Scooletz <scooletz@gmail.com>
2016-01-06 23:29:12 +0100

    StructSizeCounter able to estimate of size of any reasonably size-bound managed struct

---
+++ src/RampUp.Tests/Actors/Impl/StructSizeCounterTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using RampUp.Actors.Impl;
+
+namespace RampUp.Tests.Actors.Impl
+{
+    public class StructSizeCounterTests
+    {
+        private readonly StructSizeCounter _counter;
+
+        public StructSizeCounterTests()
+        {
+            _counter = new StructSizeCounter();
+        }
+
+        public struct SimpleBlittable
+        {
+            public int Value;
+            public Guid G;
+            public byte SomeByte;
+        }
+
+        public struct WithNullables
+        {
+            public int? Value;
+            public Guid? G;
+            public byte? SomeByte;
+        }
+
+        public struct FixedSizedBuffers
+        {
+            public unsafe fixed byte Bytes[1024];
+        }
+
+        public struct WithPointer
+        {
+            public unsafe WithPointer* Pointer;
+        }
+
+        public struct All
+        {
+            public SimpleBlittable Field1;
+            public WithNullables Fields2;
+            public FixedSizedBuffers Field3;
+            public WithPointer Field4;
+        }
+
+        public struct SomeInterface
+        {
+            public IDisposable Disposable;
+        }
+
+        [TestCase(typeof(SimpleBlittable))]
+        [TestCase(typeof(WithNullables))]
+        [TestCase(typeof(FixedSizedBuffers))]
+        [TestCase(typeof(WithPointer))]
+        [TestCase(typeof(All))]
+        [TestCase(typeof(string), ExpectedException = typeof(ArgumentException))]
+        [TestCase(typeof(SomeInterface), ExpectedException = typeof(ArgumentException))]
+        public void Size(Type t)
+        {
+            Console.WriteLine(_counter.GetSize(t));
+        }
+    }
+}
---
+++ src/RampUp/Actors/Impl/StructSizeCounter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace RampUp.Actors.Impl
+{
+    public class StructSizeCounter
+    {
+        private const int ProbeSize = 10 * 1024;
+        private readonly Dictionary<Type, int> _sizes = new Dictionary<Type, int>();
+
+        public int GetSize(Type t)
+        {
+            int size;
+            if (_sizes.TryGetValue(t, out size))
+            {
+                return size;
+            }
+
+            Assert(t);
+
+            var dm = new DynamicMethod("GetSizeOf" + t.Namespace.Replace(".", "_") + t.Name, typeof(int), Type.EmptyTypes,
+                t.Assembly.Modules.First(), true);
+            var il = dm.GetILGenerator();
+            il.DeclareLocal(typeof(byte*));
+
+            il.Emit(OpCodes.Ldc_I4, ProbeSize);
+            il.Emit(OpCodes.Conv_U);
+            il.Emit(OpCodes.Localloc);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            var initBytes = typeof(StructSizeCounter).GetMethod("InitBytes", BindingFlags.Static | BindingFlags.NonPublic);
+            il.EmitCall(OpCodes.Call, initBytes, null);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Initobj, t);
+            il.Emit(OpCodes.Ldloc_0);
+            var countZeroes = typeof(StructSizeCounter).GetMethod("CountZeros", BindingFlags.Static | BindingFlags.NonPublic);
+            il.EmitCall(OpCodes.Call, countZeroes, null);
+            il.Emit(OpCodes.Ret);
+
+            size = ((Func<int>)dm.CreateDelegate(typeof(Func<int>)))();
+            _sizes[t] = size;
+            return size;
+        }
+
+        private void Assert(Type t)
+        {
+            if (t.IsValueType == false && t.IsPointer == false)
+            {
+                throw new ArgumentException($"This counts size only for value types. {t.FullName} is not a value type.");
+            }
+
+            var fieldTypes = t.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .Select(fi => fi.FieldType)
+                .Distinct();
+
+            foreach (var ft in fieldTypes)
+            {
+                if (ft.IsValueType || ft.IsPointer)
+                {
+                    if (ft.IsPrimitive == false)
+                    {
+                        GetSize(ft);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException($"This counts size only for value types. {t.FullName} is not a value type.");
+                }
+            }
+        }
+
+        private static unsafe int CountZeros(byte* bytes)
+        {
+            var size = 0;
+            for (var i = 0; i < ProbeSize; i++)
+            {
+                size += 1 - bytes[i];
+            }
+            return size;
+        }
+
+        private static unsafe void InitBytes(byte* bytes)
+        {
+            for (var i = 0; i < ProbeSize; i++)
+            {
+                bytes[i] = 1;
+            }
+        }
+
+        /// <summary>
+        /// Just for picking up IL in disassembler.
+        /// </summary>
+        private struct A
+        {
+            public int Value;
+            public Guid OtherValue;
+        }
+
+        /// <summary>
+        /// Just for picking up IL in disassembler.
+        /// </summary>
+        /// <returns></returns>
+        private static unsafe int GetManagedSize()
+        {
+            byte* bytes = stackalloc byte[ProbeSize];
+
+            // first copy 1s to bytes
+            InitBytes(bytes);
+
+            *(A*)bytes = default(A);
+
+            // count zeroes as .NET structs are zero initialized
+            return CountZeros(bytes);
+        }
+    }
+}
