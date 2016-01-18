07c20e3ebf190132c01d1b76dc75d83ae92ba802
Scooletz <scooletz@gmail.com>
2016-01-18 19:07:01 +0100

    PerfomanceTests & buffer descriptor bug fix

--- src/RampUp.Tests/PerformanceTests.cs
+++ src/RampUp.Tests/PerformanceTests.cs
@@ -2,11 +2,14 @@
 using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using RampUp.Actors;
 using RampUp.Actors.Impl;
 using RampUp.Buffers;
 using RampUp.Ring;
+using RampUp.Threading;
 
 namespace RampUp.Tests
 {
@@ -28,7 +31,8 @@ namespace RampUp.Tests
         [Test]
         public void Actors()
         {
-            using (var buffer = new ManyToOneRingBuffer(new UnsafeBuffer(1024.Megabytes() + RingBufferDescriptor.TrailerLength)))
+            using (var buffer =
+                new ManyToOneRingBuffer(new UnsafeBuffer(512.Megabytes() + RingBufferDescriptor.TrailerLength)))
             {
                 buffer.Write(5, new ByteChunk());
                 buffer.Read((a, b) => { }, 1);
@@ -36,8 +40,9 @@ namespace RampUp.Tests
                 var module = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("ActorsTestsDynAssembly"),
                     AssemblyBuilderAccess.Run).DefineDynamicModule("main");
                 var counter = new StructSizeCounter();
-                var registry = new ActorRegistry(new[] { Tuple.Create((IActor)new Handler(), (IRingBuffer)buffer, new ActorId(1)) });
-                var writer = BaseMessageWriter.Build(counter, registry.GetMessageTypeId, new[] { typeof(A) }, module);
+                var registry =
+                    new ActorRegistry(new[] {Tuple.Create((IActor) new Handler(), (IRingBuffer) buffer, new ActorId(1))});
+                var writer = BaseMessageWriter.Build(counter, registry.GetMessageTypeId, new[] {typeof (A)}, module);
 
                 var bus = new Bus(new ActorId(2), registry, 1, writer);
 
@@ -45,14 +50,55 @@ namespace RampUp.Tests
                 bus.Publish(ref msg);
 
                 // publication is prepared do it
-                var sw = Stopwatch.StartNew();
-                var howMany = 20.Megabytes();
+
+                var howMany = 200000000;
+                var oneMiliion = 1000000;
+
+                using (var scheduler = new RoundRobinThreadAffinedTaskScheduler(4))
+                {
+                    var wait = new ManualResetEventSlim();
+                    var t = new CancellationToken();
+
+                    var p1 = CreateProducer(wait, t, howMany, bus, scheduler);
+                    var p2 = CreateProducer(wait, t, howMany, bus, scheduler);
+                    var producentCount = 2;
+                    var totalMessageCount = howMany*producentCount;
+                    var consumer = Task.Factory.StartNew(() =>
+                    {
+                        var c = 0;
+                        wait.Wait(t);
+                        while (c < totalMessageCount)
+                        {
+                            buffer.Read((msgdI, chunk) => { c++; }, 10000);
+                        }
+                    }, t, TaskCreationOptions.LongRunning, scheduler);
+
+                    var sw = Stopwatch.StartNew();
+                    wait.Set();
+
+                    Task.WaitAll(p1, p2, consumer);
+
+                    sw.Stop();
+                    Console.WriteLine(
+                        $"{howMany} messages written by each of {producentCount} producers and consumed in {sw.Elapsed}");
+                    Console.WriteLine(
+                        $"One million in {TimeSpan.FromMilliseconds(sw.Elapsed.TotalMilliseconds*oneMiliion/totalMessageCount)}");
+                }
+            }
+        }
+
+        private static Task CreateProducer(ManualResetEventSlim start, CancellationToken t, int howMany, Bus bus,
+            RoundRobinThreadAffinedTaskScheduler scheduler)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var a = new A();
+                start.Wait(t);
                 for (var i = 0; i < howMany; i++)
                 {
-                    bus.Publish(ref msg);
+                    bus.Publish(ref a);
                 }
-                Console.WriteLine($"{howMany} messages written in {sw.Elapsed}");
-            }
+            }, t, TaskCreationOptions.LongRunning, scheduler);
         }
     }
 }
--- src/RampUp/Ring/RingBufferDescriptor.cs
+++ src/RampUp/Ring/RingBufferDescriptor.cs
@@ -18,17 +18,17 @@ namespace RampUp.Ring
         /// <summary>
         /// Offset within the record at which the message type field begins.
         /// </summary>
-        public static readonly int TypeOffset = LengthOffset + sizeof(int);
+        public static readonly int TypeOffset = LengthOffset + sizeof (int);
 
         /// <summary>
         /// Length of the record header in bytes.
         /// </summary>
-        public static readonly int HeaderLength = sizeof(int) * 2;
+        public const int HeaderLength = sizeof (int)*2;
 
         /// <summary>
         /// Alignment as a multiple of bytes for each record.L
         /// </summary>
-        public static readonly int RecordAlignment = HeaderLength;
+        public const int RecordAlignment = HeaderLength;
 
         /// <summary>
         /// Offset within the trailer for where the tail value is stored.
@@ -63,22 +63,22 @@ namespace RampUp.Ring
         static RingBufferDescriptor()
         {
             var offset = 0;
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             TailPositionOffset = offset;
 
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             HeadCachePositionOffset = offset;
 
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             HeadPositionOffset = offset;
 
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             CorrelationCounterOffset = offset;
 
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             ConsumerHeartbeatOffset = offset;
 
-            offset += Native.CacheLineSize * 2;
+            offset += Native.CacheLineSize*2;
             TrailerLength = offset;
         }
 
@@ -89,14 +89,15 @@ namespace RampUp.Ring
          * @throws IllegalStateException if the buffer capacity is incorrect.
          */
 
-            ///<summary>
-            /// Checks the buffer capacity
-            /// </summary>
+        ///<summary>
+        /// Checks the buffer capacity
+        /// </summary>
         public static void EnsureCapacity(IUnsafeBuffer buffer)
         {
             if ((buffer.Size - TrailerLength).IsPowerOfTwo() == false)
             {
-                throw new ArgumentException("Size must be a positive power of 2 + TrailerLength, but size=" + buffer.Size);
+                throw new ArgumentException("Size must be a positive power of 2 + TrailerLength, but size=" +
+                                            buffer.Size);
             }
         }
 
@@ -108,19 +109,23 @@ namespace RampUp.Ring
             }
         }
 
-        public static long MakeHeader(int length, int messageTypeId)
+        public static unsafe long MakeHeader(int length, int messageTypeId)
         {
-            return ((long)messageTypeId << 32) | length;
+            long header = 0;
+            var i = (int*) &header;
+            *i = length;
+            *(i + 1) = messageTypeId;
+            return header;
         }
 
         public static int RecordLength(long header)
         {
-            return (int)header;
+            return (int) header;
         }
 
         public static int MessageTypeId(long header)
         {
-            return (int)(header >> 32);
+            return (int) (header >> 32);
         }
 
         public static int EncodedMsgOffset(int recordOffset)
