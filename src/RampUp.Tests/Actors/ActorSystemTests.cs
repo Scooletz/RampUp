45b1c081abf8e4e6e9e7d653d26831cceace84c0
Szymon Kulec <scooletz@gmail.com>
2016-03-30 22:48:47 +0200

    Actor model: intial implementation of ActorSystem provided with one functional test (BruceLeePingPong). It might need some polishing though.

---
+++ src/RampUp.Tests/Actors/ActorSystemTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using RampUp.Actors;
+
+namespace RampUp.Tests.Actors
+{
+    public class ActorSystemTests
+    {
+        [Test]
+        public void BruceLeePingPong()
+        {
+            var system = new ActorSystem();
+            IBus bus = null;
+            var bruce = new Bruce();
+            var lee = new Lee();
+            system.Add(bruce, ctx => { bus = ctx.Actor.Bus = ctx.Bus; });
+            system.Add(lee, ctx => { ctx.Actor.Bus = ctx.Bus; });
+            system.Start();
+
+            var p = new Pong();
+            bus.Publish(ref p);
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            system.Stop();
+
+            Assert.Less(0, bruce.Counter);
+            Assert.Less(0, lee.Counter);
+
+            Console.WriteLine($"Bruce ponged {bruce.Counter} times");
+            Console.WriteLine($"Lee pinged {lee.Counter} times");
+        }
+
+        public class Bruce : IHandle<Ping>
+        {
+            public IBus Bus;
+            public int Counter;
+
+            public void Handle(ref Envelope envelope, ref Ping msg)
+            {
+                var p = new Pong();
+                Bus.Publish(ref p);
+                Counter += 1;
+            }
+        }
+
+        public class Lee : IHandle<Pong>
+        {
+            public IBus Bus;
+            public int Counter;
+
+            public void Handle(ref Envelope envelope, ref Pong msg)
+            {
+                var p = new Ping();
+                Bus.Publish(ref p);
+                Counter += 1;
+            }
+        }
+
+        public struct Pong : IMessage
+        {
+        }
+
+        public struct Ping : IMessage
+        {
+        }
+    }
+}
--- src/RampUp/Actors/Impl/Runner.cs
+++ src/RampUp/Actors/Impl/Runner.cs
@@ -9,7 +9,8 @@ namespace RampUp.Actors.Impl
     /// </summary>
     public sealed class Runner
     {
-        private readonly IRingBuffer _buffer;
+        public readonly IRingBuffer Buffer;
+        public readonly ActorDescriptor Descriptor;
         private readonly int _batchSize;
         private readonly MessageHandler _handler;
         private readonly IBatchAware _batchAware;
@@ -17,17 +18,19 @@ namespace RampUp.Actors.Impl
         public Runner(IRingBuffer buffer, IStructSizeCounter counter, Func<Type, int> messageIdGetter, int batchSize,
             params IActor[] actors)
         {
-            _buffer = buffer;
+            Buffer = buffer;
             _batchSize = batchSize;
             if (actors.Length > 1)
             {
                 var actor = new CompositeActor(actors, counter, messageIdGetter);
                 _batchAware = actor;
                 _handler = actor.MessageHandler;
+                Descriptor = actor.Descriptor;
             }
             else
             {
                 _handler = new MessageReader(actors[0], counter, messageIdGetter).MessageHandlerImpl;
+                Descriptor = new ActorDescriptor(actors[0]);
                 _batchAware = actors[0] as IBatchAware;
             }
         }
@@ -35,7 +38,7 @@ namespace RampUp.Actors.Impl
         public void SpinOnce(out BatchInfo info)
         {
             var start = Stopwatch.GetTimestamp();
-            var processed = _buffer.Read(_handler, _batchSize);
+            var processed = Buffer.Read(_handler, _batchSize);
             var stop = Stopwatch.GetTimestamp();
 
             info = new BatchInfo(_batchSize, processed, stop - start);
