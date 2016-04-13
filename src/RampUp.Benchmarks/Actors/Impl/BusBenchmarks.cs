855211fca13bb2795fd1339c2bc190d4d1d7fc42
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-04-13 23:52:29 +0300

    Add benchmark harness (#21)
    * Add an empty console app for benchmarks
    
    * Install BenchmarkDotNet package
    
    * Install Padded.Fody
    
    * Add sample benchmark. It doesn't work so far.
    
    * 64bit by default
    
    * Weave the RampUp.Enveloper

---
+++ src/RampUp.Benchmarks/Actors/Impl/BusBenchmarks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using BenchmarkDotNet.Attributes;
+
+using Padded.Fody;
+
+using RampUp.Actors;
+using RampUp.Actors.Impl;
+using RampUp.Buffers;
+using RampUp.Ring;
+
+namespace RampUp.Benchmarks.Actors.Impl
+{
+    public class BusBenchmarks
+    {
+        private ManyToOneRingBuffer _buffer;
+        private Bus _bus;
+
+        [Setup]
+        public unsafe void Setup()
+        {
+            _buffer = new ManyToOneRingBuffer(new UnsafeBuffer(1024.Megabytes() + RingBufferDescriptor.TrailerLength));
+            var someBytes = stackalloc byte[1];
+            _buffer.Write(5, new ByteChunk(someBytes, 1));
+            _buffer.Read((a, b) => { }, 1);
+
+            var module = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                new AssemblyName("ActorsTestsDynAssembly"),
+                AssemblyBuilderAccess.Run).DefineDynamicModule("main");
+            var counter = new StructSizeCounter();
+            var registry =
+                new ActorRegistry(
+                    new[]
+                        { Tuple.Create(new ActorDescriptor(new Handler()), (IRingBuffer)_buffer, new ActorId(1)) });
+            var writer = BaseMessageWriter.Build(counter, registry.GetMessageTypeId, new[] { typeof(A) }, module);
+
+            _bus = new Bus(new ActorId(2), registry, 20, writer);
+
+        }
+
+        [Benchmark]
+        public void BusPublish()
+        {
+            var msg = new A();
+            _bus.Publish(ref msg);
+        }
+
+        public struct A : IMessage
+        {
+            public int Value;
+        }
+
+        [Padded]
+        public sealed class Handler : IHandle<A>
+        {
+            public void Handle(ref Envelope envelope, ref A msg)
+            {
+            }
+        }
+    }
+}
---
+++ src/RampUp.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+using RampUp.Benchmarks.Actors.Impl;
+
+namespace RampUp.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BusBenchmarks>();
+        }
+    }
+}
