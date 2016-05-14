20e400285165270b6f2df8bc2092915e05c1a6d8
Szymon Kulec <scooletz@gmail.com>
2016-05-15 00:41:36 +0200

    OneToOneRingBuffer implemented (#25)

---
+++ src/RampUp.Tests/Categories.cs
@@ -0,0 +1,10 @@
+﻿namespace RampUp.Tests
+{
+    public static class Categories
+    {
+        /// <summary>
+        /// CodeCop based tests are excluded by default (see RampUp.sln.DotSettings file) as they conflict with other tests using <see cref="RampUp.Atomics"/>.
+        /// </summary>
+        public const string CodeCop = "CodeCop";
+    }
+}
